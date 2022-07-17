3fc5df71031de4df5105d8dff1ba82cb6c9ab850
pawali <pawali@binaracademy.org>
2022-07-17 20:55:09 +0700

    Edit app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "OK" });
+    res.status(200).json({ message: "Edit OK" });
 });
 
 app.listen(PORT, () => console.log("Server running on port", PORT))
