bc7db7cdac08effe51fffeca93ebac62cebcc555
pawali <pawali@binaracademy.org>
2022-07-17 22:10:47 +0700

    Cobain kalo error

--- app.test.js
+++ app.test.js
@@ -6,7 +6,7 @@ describe("Running test", () => {
     test("Test GET endpoint /", (done) => {
         request.get("/").then(res => {
             expect(res.status).toBe(200)
-            expect(res.body).toHaveProperty("message")
+            expect(res.body).toHaveProperty("messag")
             done()
         }).catch(err => done(err))
     })
